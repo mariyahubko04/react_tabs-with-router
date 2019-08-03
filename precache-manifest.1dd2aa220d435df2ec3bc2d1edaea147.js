814880a6261c4f888ad2a416215b59a1050b297e
unknown <mariia>
2019-08-03 23:07:19 +0300

    Updates

---
+++ precache-manifest.1dd2aa220d435df2ec3bc2d1edaea147.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc07eca7f0242ca6442e81c1bd41229e",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "e77c0c0edbe7592f6306",
+    "url": "/react_tabs/static/css/main.f4970c56.chunk.css"
+  },
+  {
+    "revision": "89d01c4d6d16341e83c1",
+    "url": "/react_tabs/static/js/2.6961691d.chunk.js"
+  },
+  {
+    "revision": "e77c0c0edbe7592f6306",
+    "url": "/react_tabs/static/js/main.c741addd.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
